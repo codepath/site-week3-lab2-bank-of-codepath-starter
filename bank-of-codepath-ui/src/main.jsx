826acf683972bb0e2ffe24aba6219e4af12292e2
Yilika Loufoua <yilika.loufoua@tcu.edu>
2023-03-07 02:27:24 -0600

    Remove TDD and upgrade deps

--- bank-of-codepath-ui/src/main.jsx
+++ bank-of-codepath-ui/src/main.jsx
@@ -1,37 +1,14 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
-import { InstantNoodles } from "instant-noodles"
-import App from "./components/App/App"
-import "./globals.css"
-
-import { prepareMockServiceWorker, config } from "./tests/setup"
-import { testApp } from "./tests/feature-000-app.test"
-import { testNavbar } from "./tests/feature-001-navbar.test"
-import { testHome } from "./tests/feature-002-home.test"
-import { testAddTransaction } from "./tests/feature-003-add-transaction.test"
-import { testBankActivity } from "./tests/feature-004-bank-activity.test"
-import { testTransactionDetail } from "./tests/feature-005-transaction-detail.test"
-
-const tests = {
-  app: testApp,
-  navbar: testNavbar,
-  home: testHome,
-  addTransaction: testAddTransaction,
-  bankActivity: testBankActivity,
-  transactionDetail: testTransactionDetail,
-}
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./components/App/App";
+import "./globals.css";
 
 const renderApp = () =>
   ReactDOM.render(
     <React.StrictMode>
       <App />
-      {/* Leave this here for live test environment */}
-      <InstantNoodles RootComponent={App} tests={tests} config={config} />
     </React.StrictMode>,
     document.getElementById("root")
-  )
+  );
 
-/* Comment out this next line to run against live Express API */
-prepareMockServiceWorker().then(() => renderApp())
-/* Uncomment this next line to run against live Express API */
-// renderApp()
+renderApp();
