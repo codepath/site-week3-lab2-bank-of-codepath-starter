77130ff589d53dfdaca1f0ba2be09bb4016f8d19
jamjamgobambam <jamila.khawaja85@gmail.com>
2023-06-03 16:45:18 -0400

    update main to use createRoot

--- bank-of-codepath-ui/src/main.jsx
+++ bank-of-codepath-ui/src/main.jsx
@@ -1,14 +1,10 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./components/App/App";
-import "./globals.css";
+import * as React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/App/App'
+import './globals.css'
 
-const renderApp = () =>
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-
-renderApp();
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
